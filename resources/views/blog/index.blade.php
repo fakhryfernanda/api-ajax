b30c8c7b4c056f2fe780df4694de6c8313faf264
fakhryfernanda <mff023@gmail.com>
2022-12-20 16:03:56 +0700

    Create view for blog

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,5 +1,12 @@
 @extends('layout')
 
 @section('content')
-<h1>Blog</h1>
+<h1 class="text-4xl">Blog</h1>
+<table>
+    <tr>
+        <th class="px-2 py-1 border border-slate-500">Title</th>
+        <th class="px-2 py-1 border border-slate-500">Image</th>
+        <th class="px-2 py-1 border border-slate-500">Author</th>
+    </tr>
+</table>
 @endsection
