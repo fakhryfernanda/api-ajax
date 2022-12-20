048ee17acc1841e2f5fcc1de19026510fe7bda46
fakhryfernanda <mff023@gmail.com>
2022-12-20 15:43:50 +0700

    Menambahkan route all dan detail

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index()
+    public function all()
     {
         $posts = Post::all();
         return response()->json([
