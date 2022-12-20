5c2a360b53c1052e9f998a97363f1ea9b665ee61
fakhryfernanda <mff023@gmail.com>
2022-12-20 15:42:11 +0700

    Membuat method index dan detail pada controller post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,32 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::all();
+        return response()->json([
+            "status" => true,
+            "message" => "",
+            "data" => $posts
+        ]);
+    }
+
+    public function detail($id)
+    {
+        $post = Post::find($id);
+
+        if ($blog == null) {
+            return response()->json([
+                "status" => false,
+                "message" => "Post tidak ditemukan",
+                "data" => null
+            ]);
+        }
+
+        return response()->json([
+            "status" => true,
+            "message" => "",
+            "data" => $post
+        ]);
+    }
 }
